ef5211783d7a234f8db441dc63a405c2cca5a251
Derek Jones <lab@derekj.dev>
2023-01-13 11:15:41 -0500

    Prevent index out of range if using large shift number

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,6 +4,15 @@ from alphabet_list import alphabet
 from art import logo
 
 
+print(logo)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+shift = shift % 26
+
+
 def caesar(start_text, shift_amount, cypher_direction):
     end_text = ""
 
@@ -14,14 +23,6 @@ def caesar(start_text, shift_amount, cypher_direction):
         end_text += alphabet[alphabet.index(letter) + shift_amount]
 
     print(f"The {cypher_direction}d text is: {end_text}")
-
-
-print(logo)
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
 caesar(start_text=text, shift_amount=shift, cypher_direction=direction)

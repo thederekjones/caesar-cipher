743faea5029865fd4a8fb832af510d48736eda1f
Derek Jones <lab@derekj.dev>
2023-01-13 00:04:47 -0500

    Add decrypt function to decode the encoded text

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from alphabet_list import alphabet
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -6,12 +8,26 @@ shift = int(input("Type the shift number:\n"))
 
 
 def encrypt(plain_text, shift_amount):
-    cipher_text = ""
+    encrypted_text = ""
 
     for letter in plain_text:
-        cipher_text += alphabet[alphabet.index(letter) + shift_amount]
+        encrypted_text += alphabet[alphabet.index(letter) + shift_amount]
 
-    print(f"The encoded text is {cipher_text}")
+    print(f"The encoded text is: {encrypted_text}")
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(plain_text, shift_amount):
+    decrypted_text = ""
+
+    for letter in plain_text:
+        decrypted_text += alphabet[alphabet.index(letter) - shift_amount]
+
+    print(f"The decrypted text is: {decrypted_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
+else:
+    print("That wasn't a valid choice!")

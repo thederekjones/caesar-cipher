dfbe1e107d3a5dc781d98191410255b339a7b91c
Derek Jones <lab@derekj.dev>
2023-01-14 12:25:45 -0500

    Add ability to choose to restart cypher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -6,12 +6,6 @@ from art import logo
 
 print(logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-shift = shift % 26
-
 
 def caesar(start_text, shift_amount, cypher_direction):
     end_text = ""
@@ -19,13 +13,28 @@ def caesar(start_text, shift_amount, cypher_direction):
     if cypher_direction == "decode":
         shift_amount *= -1
 
-    for letter in start_text:
-        if letter in alphabet:
-            end_text += alphabet[alphabet.index(letter) + shift_amount]
+    for char in start_text:
+        if char in alphabet:
+            end_text += alphabet[alphabet.index(char) + shift_amount]
         else:
-            end_text += letter
+            end_text += char
 
     print(f"The {cypher_direction}d text is: {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, cypher_direction=direction)
+restart_cipher = True
+
+while restart_cipher:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cypher_direction=direction)
+
+    restart = input(
+        "Do you want to restart the program? Type 'yes' or 'no' now.\n"
+    ).lower()
+
+    if restart == "no":
+        restart_cipher = False
